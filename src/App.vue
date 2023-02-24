07b245bfb2367c13fedfe74a14f5ea677cf7f6c7
TheJeffRicardo <konkemalindi@gmail.com>
2023-02-24 10:23:06 +0200

    counter add and subtract

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container bg-secondary">
+    <div class="row">
+      <div class="col">
+        <div class="form">
+          <div>
+            <input type="text" placeholder="0" :value="counter" readonly>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <button class="btn btn-danger" @click.prevent="decrease">Decrease</button>
+          </div>
+          <div class="col">
+            <button class="btn btn-success" @click.prevent="increase">Increase</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  //How we access our state it must be returned
+  computed: {
+    counter(){
+      return this.$store.state.counter
+    }
+  },
+  //Any component that has been displayed on the browser
+  // unmounted - when a component is remove
+  methods: {
+    increase() {
+      this.$store.dispatch('increaseCounter')
+    },
+    decrease() {
+      this.$store.dispatch('decreaseCounter')
+    }
   }
 }
 </script>
